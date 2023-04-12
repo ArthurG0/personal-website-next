382d0e62791267b3e79e4ec059dac2776e423b14
ArthurG0 <agatin@umail.ucsb.edu>
2023-04-11 19:49:40 -0700

    adding google verification tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="M3CgfirjXNlS6may20KU1TKmxY3h1Omz9WCZzLKQJ6Y" />
+      </Head>
       <body>
         <Main />
         <NextScript />
