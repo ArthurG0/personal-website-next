8e88b3f673cb29ae96ea91741f1b7cf183e32468
ArthurG0 <agatin@umail.ucsb.edu>
2023-05-15 02:00:59 -0700

    google crawler meta tags and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <title>Arthur Gatin's Homepage</title>
+      <meta
+        name="description"
+        key="desc"
+        content="Arthur's Website Content"
+      ></meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
 }
--- src/pages/startdate.tsx
+++ src/pages/startdate.tsx
@@ -1,4 +1,5 @@
 import styles from '../styles/Startdate.module.css'
+import Head from 'next/head'
 
 
 import React, { useState , useEffect, useRef} from 'react'
@@ -51,15 +52,25 @@ export default function StartDate() {
 
     
     return (
-        <div style={{textAlign: 'center', paddingTop: '10vh'}}>
-            <h3 className={styles.smallerText}>There are</h3>
-            <h1 className={styles.largerText}>{days_left}</h1>
-            <h3 className={styles.smallerText}>days left until my start date, {readable_start_date}</h3>
-            <Link className={styles.homeIcon} href="/">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32px" height="32px">
-                    <path d="M 16 2.59375 L 15.28125 3.28125 L 2.28125 16.28125 L 3.71875 17.71875 L 5 16.4375 L 5 28 L 14 28 L 14 18 L 18 18 L 18 28 L 27 28 L 27 16.4375 L 28.28125 17.71875 L 29.71875 16.28125 L 16.71875 3.28125 Z M 16 5.4375 L 25 14.4375 L 25 26 L 20 26 L 20 16 L 12 16 L 12 26 L 7 26 L 7 14.4375 Z"/>
-                </svg>
-            </Link>
-        </div>
+        <>
+            <Head>
+                <title>Countdown to Arthur's Start Date</title>
+                <meta
+                    name="description"
+                    key="desc"
+                    content="Countdown to Arthur's start date at Amazon"
+                ></meta>
+            </Head>
+            <div style={{textAlign: 'center', paddingTop: '10vh'}}>
+                <h3 className={styles.smallerText}>There are</h3>
+                <h1 className={styles.largerText}>{days_left}</h1>
+                <h3 className={styles.smallerText}>days left until my start date, {readable_start_date}</h3>
+                <Link className={styles.homeIcon} href="/">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32px" height="32px">
+                        <path d="M 16 2.59375 L 15.28125 3.28125 L 2.28125 16.28125 L 3.71875 17.71875 L 5 16.4375 L 5 28 L 14 28 L 14 18 L 18 18 L 18 28 L 27 28 L 27 16.4375 L 28.28125 17.71875 L 29.71875 16.28125 L 16.71875 3.28125 Z M 16 5.4375 L 25 14.4375 L 25 26 L 20 26 L 20 16 L 12 16 L 12 26 L 7 26 L 7 14.4375 Z"/>
+                    </svg>
+                </Link>
+            </div>
+        </>
     )
 }
