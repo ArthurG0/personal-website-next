939a627ed999f938367b707ace80a619d6e47faf
ArthurG0 <agatin@umail.ucsb.edu>
2023-06-21 23:31:04 -0700

    23 year old now

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -43,7 +43,7 @@ function HomeComponent(props: any) {
                 </a>
                     
             </div>
-            <div className={styles.HomeAbout}>I am a 22-year old Russian-American Software Developer.</div>
+            <div className={styles.HomeAbout}>I am a 23-year old Russian-American Software Developer.</div>
             <div className={styles.HomeAbout}>I have graduated from University of California, Santa Barbara as a Bachelor of Computer Science. I am currently enrolled in a 5-year Master's Program at UCSB.</div>
             {/* <div className="HomeAbout">I am looking for work.</div> */}
             <div className={styles.HomeSection}>Contact me!
