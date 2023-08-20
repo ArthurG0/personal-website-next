cd28ba9bcaf2deb8084ed587ce417e5130ab1ca8
ArthurG0 <agatin@umail.ucsb.edu>
2023-08-20 02:01:00 -0700

    added a go-back button for russian typer

--- src/components/RussianTyper.tsx
+++ src/components/RussianTyper.tsx
@@ -4,6 +4,7 @@ import React, { useState , useEffect } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ENGLISH_TO_RUSSIAN_KEYBOARD } from '../constants/russian-conversion-table'
+import Link from 'next/link';
 
 
 function RussianTyper(props: none) {
@@ -79,6 +80,14 @@ function RussianTyper(props: none) {
     const transformedText = transformEnglishToRussian(typedText)
 
 
+    const returnHomeElement = 
+        <Link className={styles.homeIcon} href="/">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32px" height="32px">
+                <path d="M 16 2.59375 L 15.28125 3.28125 L 2.28125 16.28125 L 3.71875 17.71875 L 5 16.4375 L 5 28 L 14 28 L 14 18 L 18 18 L 18 28 L 27 28 L 27 16.4375 L 28.28125 17.71875 L 29.71875 16.28125 L 16.71875 3.28125 Z M 16 5.4375 L 25 14.4375 L 25 26 L 20 26 L 20 16 L 12 16 L 12 26 L 7 26 L 7 14.4375 Z"/>
+            </svg>
+        </Link>
+
+
 
     return (
         <div style={{textAlign: 'center', paddingTop: '10vh'}}>
@@ -86,7 +95,7 @@ function RussianTyper(props: none) {
             <p style={{maxWidth: '40%', margin: 'auto', padding: '2vh 0'}} id='instr-2'>Type something using English letters, and it will as if you're typing on a Russian keyboard!</p>
             <form>
 
-            <textarea className={styles.inputBox} id="inputBox" rows="5" cols="50" type='text' autoFocus value={typedText} onChange={handleChange}></textarea>
+            <textarea className={styles.inputBox} id="inputBox" rows="5" cols="50" type='text' autoFocus value={typedText} onChange={handleChange} placeholder="Enter text here:"></textarea>
             <br></br>
             <textarea className={styles.inputBox} id="copyBox" rows="5" cols="50" type='text' readOnly value={transformedText}></textarea>
             <br></br>
@@ -94,6 +103,7 @@ function RussianTyper(props: none) {
             <input className={styles.Button} type="button" value="Copy transformed" onClick={copyTransformed}/>
             </form>
             <ToastContainer/>
+            {returnHomeElement}
         </div>
     )
 
