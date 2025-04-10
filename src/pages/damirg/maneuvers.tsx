9811f95e13a1a92e9e7fb9fd1a79b4c76ac43423
ArthurG0 <agatin@umail.ucsb.edu>
2025-04-10 00:23:06 -0400

    improvements, scroll, 0.9rem labels

--- src/pages/damirg/maneuvers.tsx
+++ src/pages/damirg/maneuvers.tsx
@@ -8,24 +8,52 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export default function ManeuversComponent() {
 
+    interface OptionCodes {
+
+    }
+
     const [selectedTab, setSelectedTab] = useState(0)
     const [finalText, setFinalText] = useState("Here are my maneuvers: A, B, C")
-    const [selectedRadios, setSelectedRadios] = useState({
+    const [selectedRadios, setSelectedRadios] = useState<OptionCodes>({
         barrel_roll: false,
         roll: false,
         cobra: false,
         instrument_1: false,
         instrument_2: false,
         instrument_3: false,
+        instrument_4: false,
+        instrument_5: false,
+        instrument_6: false,
     })
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        // Function to check if the device is mobile
+        const checkIfMobile = () => {
+        const width = window.innerWidth;
+        setIsMobile(width < 768); // You can adjust this threshold as needed
+        };
+
+        // Add event listener for window resize
+        window.addEventListener('resize', checkIfMobile);
+
+        // Initial check on component mount
+        checkIfMobile();
+
+        // Cleanup function to remove event listener
+        return () => window.removeEventListener('resize', checkIfMobile);
+    }, []);
 
     const radioIdToReadableManeuver: Record<string, string> = {
         'barrel_roll': 'Barrel Roll',
         'roll': 'Roll',
         'cobra': 'Cobra Maneuver',
         'instrument_1': 'Instrument Maneuver 1',
-        'instrument_2': 'Instrument Maneuver 2',
+        'instrument_2': 'Maneuver 2',
         'instrument_3': 'Instrument Maneuver 3',
+        'instrument_4': 'Instrument Maneuver 4',
+        'instrument_5': 'Maneuver 5',
+        'instrument_6': 'Instrument Maneuver 6',
     }
 
     useEffect(() => {
@@ -80,9 +108,17 @@ export default function ManeuversComponent() {
         });
     }
 
+    let sxProperty = null
+    if (isMobile) {
+        sxProperty = {'.MuiTypography-root': {fontSize: '0.9rem'}}
+    }
+
 
     function CustomTabPanel(props: any) {
         const { children, value, index, ...other } = props;
+
+        let paddingValue = 3
+        if (isMobile) paddingValue = 1
       
         return (
           <div
@@ -92,7 +128,7 @@ export default function ManeuversComponent() {
             aria-labelledby={`simple-tab-${index}`}
             {...other}
           >
-            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+            {value === index && <Box sx={{ p: paddingValue, overflowX: 'scroll' }}>{children}</Box>}
           </div>
         );
     }
@@ -117,11 +153,12 @@ export default function ManeuversComponent() {
             </Tabs>
             </Box>
             <CustomTabPanel value={selectedTab} index={0}>
-                <FormGroup className={styles.FormGroup}>
+                <FormGroup>
                     <FormControlLabel
                         control={
                         <Checkbox checked={selectedRadios.barrel_roll} onChange={handleCheckboxChange} name="barrel_roll" />
                         }
+                        sx={sxProperty}
                         label={radioIdToReadableManeuver['barrel_roll']}
 
                     />
@@ -129,6 +166,7 @@ export default function ManeuversComponent() {
                         control={
                         <Checkbox checked={selectedRadios.roll} onChange={handleCheckboxChange} name="roll" />
                         }
+                        sx={sxProperty}
                         label={radioIdToReadableManeuver['roll']}
 
                     />
@@ -136,33 +174,95 @@ export default function ManeuversComponent() {
                         control={
                         <Checkbox checked={selectedRadios.cobra} onChange={handleCheckboxChange} name="cobra" />
                         }
+                        sx={sxProperty}
                         label={radioIdToReadableManeuver['cobra']}
                     />
                 </FormGroup>
             </CustomTabPanel>
 
             <CustomTabPanel value={selectedTab} index={1}>
-            <FormGroup className={styles.FormGroup}>
-                    <FormControlLabel
+            <FormGroup sx={{maxHeight: '40vh'}}>
+                {
+                    [
+                        'instrument_1',
+                        'instrument_2',
+                        'instrument_3',
+                        'instrument_4',
+                        'instrument_5',
+                        'instrument_3',
+                        'instrument_4',
+                        'instrument_5',
+                        'instrument_2',
+                        'instrument_1',
+                        'instrument_3',
+                    ].map((option_code,index) => {
+                        return (
+                        <FormControlLabel 
+                            control={
+                                <Checkbox checked={selectedRadios[option_code as keyof OptionCodes]} onChange={handleCheckboxChange} name={option_code} />
+                            }
+                            label={radioIdToReadableManeuver[option_code]}
+                            sx={sxProperty}
+                        /> )
+                    })
+                }
+                    {/* <FormControlLabel
                         control={
                         <Checkbox checked={selectedRadios.instrument_1} onChange={handleCheckboxChange} name="instrument_1" />
                         }
                         label={radioIdToReadableManeuver['instrument_1']}
-
+                        sx={sxProperty}
                     />
                     <FormControlLabel
                         control={
                         <Checkbox checked={selectedRadios.instrument_2} onChange={handleCheckboxChange} name="instrument_2" />
                         }
                         label={radioIdToReadableManeuver['instrument_2']}
-
+                        sx={sxProperty}
                     />
                     <FormControlLabel
                         control={
                         <Checkbox checked={selectedRadios.instrument_3} onChange={handleCheckboxChange} name="instrument_3" />
                         }
+                        sx={sxProperty}
                         label={radioIdToReadableManeuver['instrument_3']}
                     />
+                    <FormControlLabel
+                        control={
+                        <Checkbox checked={selectedRadios.instrument_4} onChange={handleCheckboxChange} name="instrument_4" />
+                        }
+                        sx={sxProperty}
+                        label={radioIdToReadableManeuver['instrument_4']}
+                    />
+                    <FormControlLabel
+                        control={
+                        <Checkbox checked={selectedRadios.instrument_5} onChange={handleCheckboxChange} name="instrument_5" />
+                        }
+                        sx={sxProperty}
+                        label={radioIdToReadableManeuver['instrument_5']}
+                    />
+                    <FormControlLabel
+                        control={
+                        <Checkbox checked={selectedRadios.instrument_3} onChange={handleCheckboxChange} name="instrument_3" />
+                        }
+                        sx={sxProperty}
+                        label={radioIdToReadableManeuver['instrument_3']}
+                    />
+                    <FormControlLabel
+                        control={
+                        <Checkbox checked={selectedRadios.instrument_4} onChange={handleCheckboxChange} name="instrument_4" />
+                        }
+                        sx={sxProperty}
+                        label={radioIdToReadableManeuver['instrument_4']}
+                    />
+                    <FormControlLabel
+                        control={
+                        <Checkbox checked={selectedRadios.instrument_5} onChange={handleCheckboxChange} name="instrument_5" />
+                        }
+                        sx={sxProperty}
+                        label={radioIdToReadableManeuver['instrument_5']}
+                    /> */}
+                    
                 </FormGroup>
             </CustomTabPanel>
             
