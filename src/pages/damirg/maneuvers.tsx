68fac41c19c55836cc378477d0d9b4ce676e31c9
ArthurG0 <agatin@umail.ucsb.edu>
2025-04-13 19:43:35 -0400

    marginally increasing checkbox padding in mobile

--- src/pages/damirg/maneuvers.tsx
+++ src/pages/damirg/maneuvers.tsx
@@ -389,7 +389,7 @@ export default function ManeuversComponent() {
     if (isMobile) {
         formControlLabelSxProperty = {
             '.MuiTypography-root': { fontSize: '1rem' },
-            '.MuiButtonBase-root': { padding: '4px' }
+            '.MuiButtonBase-root': { padding: '5px' }
         }
         checkboxBoxSxProperty = {
             ...checkboxBoxSxProperty,
